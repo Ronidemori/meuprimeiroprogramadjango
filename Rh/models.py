ae5c8cb5bb418ecd0fecf1377c7d983049d01ed4
Ronidemori <ronidemori@gmail.com>
2022-07-11 20:33:43 -0300

    Versão inicial do projeto

---
+++ Rh/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+class Departamento(models.Model):
+    nome = models.CharField(max_length=20)
+    def  _str_ (self):
+        return self.nome 
+
+class Funcionario(models.Model):
+    CARGOS = [
+        ('ES', 'Estagiario'),
+        ('As', 'Assistente'),
+        ('JR', 'Junior'),
+        ('PL', 'Pleno'),
+        ('SR', 'Senior'),
+        ('GR', 'Gerente')
+    ]    
+    matricula = models.IntegerField()
+    nome = models.CharField(max_length=50)
+    cargo = models.CharField(max_length=2, choices = CARGOS)    
+    departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
+    salario = models.DecimalField(max_digits=10,decimal_places=2)
+    data_nascimento = models.DateField(null=True)
